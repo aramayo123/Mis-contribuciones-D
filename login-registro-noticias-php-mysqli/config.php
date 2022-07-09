e471bf49a223f9bd605e4b9805ecc0a429118a40
Aramayo Diego Leonel <102543974+aramayo123@users.noreply.github.com>
2022-07-09 13:09:34 -0300

    Update config.php

--- login-registro-noticias-php-mysqli/config.php
+++ login-registro-noticias-php-mysqli/config.php
@@ -4,12 +4,7 @@
     $user = "root";
     $password = "";
     $database = "tutoriales";
-    /*
-    $server = "sql300.epizy.com";
-    $user = "epiz_32074090";
-    $password = "QABHKcesgw";
-    $database = "epiz_32074090_gym";
-    */
+ 
     $conn = mysqli_connect($server,$user,$password,$database);
     /*
     if($conn){
